9cdb07cdfa4b175cf980d3448b0023c51b903f91
nishikawa-naonori <watar.wind.xyz1990@gmail.com>
2021-02-05 17:35:48 +0900

    binding.pry

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -4,10 +4,12 @@ class LikesController < ApplicationController
   before_action :post_params
 
   def create
+    binding.pry
     Like.create(user_id: current_user.id, post_id: params[:id])
   end
 
   def destroy
+    binding.pry
     Like.find_by(user_id: current_user.id, post_id: params[:id]).destroy
   end
 
