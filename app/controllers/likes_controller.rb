bbf65ecfa09b77eabe063f644b7c3a2107ab2be2
nishikawa-naonori <watar.wind.xyz1990@gmail.com>
2021-02-06 16:30:01 +0900

    これで完成

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -5,14 +5,29 @@ class LikesController < ApplicationController
 
   def create
     #binding.pry
-    Like.create(user_id: current_user.id, post_id: params[:id])
-    redirect_back(fallback_location: root_path)
+    if Like.create(user_id: current_user.id, post_id: params[:id])
+      respond_to do |format|
+        format.js
+      end
+
+    #redirect_back(fallback_location: root_path)
+    else
+        redirect_back(fallback_location: root_path)
+    end
+    #redirect_to posts_path
   end
 
   def destroy
     #binding.pry
-    Like.find_by(user_id: current_user.id, post_id: params[:id]).destroy
-    redirect_back(fallback_location: root_path)
+    if Like.find_by(user_id: current_user.id, post_id: params[:id]).destroy
+      respond_to do |format|
+        format.js
+      end
+      
+    else
+      redirect_back(fallback_location: root_path)
+    end
+    #redirect_to posts_path
   end
 
   private
