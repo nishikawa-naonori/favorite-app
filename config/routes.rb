40ff82f6c61d69af042b9005b0381dd5645f8b80
nishikawa-naonori <watar.wind.xyz1990@gmail.com>
2021-02-28 08:32:42 +0900

    Revert "取り消し"
    This reverts commit 3d312500944091f584473b9d9277c71ae23ae02e.

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,10 @@
 
 Rails.application.routes.draw do
   devise_for :users
-  root 'posts#index'
+  root 'home#index'
   resources :posts
-  resources :likes, only: [:create,:destroy], as: 'create_destroy_like'
-  #resources :likes, only: [:destroy]
-  #post 'like/:id', to: 'likes#create', as: 'create_like'
-  #post 'like/:id' => 'likes#create', as: 'create_like'
+  post 'like/:id' => 'likes#create', as: 'create_like'
 
-  #delete 'like/:id', to: 'likes#destroy', as: 'destroy_like'
-  #delete 'like/:id' => 'likes#destroy', as: 'destroy_like'
-
+  # 以下の1行を追記
+  delete 'like/:id' => 'likes#destroy', as: 'destroy_like'
 end
