31b78d8b0952a249c9467d6c69499000cff007de
nishikawa-naonori <watar.wind.xyz1990@gmail.com>
2021-02-27 17:00:19 +0900

    リファクタリング

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -4,17 +4,14 @@ class LikesController < ApplicationController
   before_action :post_params
 
   def create
-    #binding.pry
     if Like.create(user_id: current_user.id, post_id: params[:id])
       respond_to do |format|
         format.js
       end
 
-    #redirect_back(fallback_location: root_path)
     else
         redirect_back(fallback_location: root_path)
     end
-    #redirect_to posts_path
   end
 
   def destroy
