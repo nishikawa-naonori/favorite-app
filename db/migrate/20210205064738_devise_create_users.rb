bcf3e1e6cdde99f878ae52560de60d50c81e2212
nishikawa-naonori <watar.wind.xyz1990@gmail.com>
2021-03-07 22:46:53 +0900

    リファクタリング６

--- db/migrate/20210205064738_devise_create_users.rb
+++ db/migrate/20210205064738_devise_create_users.rb
@@ -7,12 +7,10 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
-      ## Recoverable
-      t.string   :reset_password_token
-      t.datetime :reset_password_sent_at
+      ## ニックネーム
+      t.string :nickname,           null: false, default: ""
 
-      ## Rememberable
-      t.datetime :remember_created_at
+
 
       ## Trackable
       # t.integer  :sign_in_count, default: 0, null: false
@@ -34,6 +32,12 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
 
 
       t.timestamps null: false
+
+      ## Rememberable
+      t.datetime :remember_created_at
+      ## Recoverable
+      t.string   :reset_password_token
+      t.datetime :reset_password_sent_at
     end
 
     add_index :users, :email,                unique: true
